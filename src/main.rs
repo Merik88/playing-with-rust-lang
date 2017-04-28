cc615a94e5666e7540735cf771a7b7a6ff04918c
Markus Eriksson <markus.eriksson@nepa.com>
2017-04-28 14:35:21 +0200

    Structures example from http://rustbyexample.com/custom_types/structs.html

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,49 @@
-use std::fmt;
+// A unit struct
+struct Nil;
 
-impl fmt::Display for Matrix {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        writeln!(f, "( {} {} )", self.0, self.1)?;
-        write!(f, "( {} {} )", self.2, self.3)
-    }
+// A tuple struct
+struct Pair(i32, f32);
+
+// A struct with two fields
+struct Point {
+    x: f32,
+    y: f32,
 }
 
-// Tuples can be used as function arguments and as return values
-fn reverse(pair: (i32, bool)) -> (bool, i32) {
-    // `let` can be used to bind the members of a tuple to variables
-    let (integer, boolean) = pair;
-
-    (boolean, integer)
+// Structs can be reused as fields of another struct
+#[allow(dead_code)]
+struct Rectangle {
+    p1: Point,
+    p2: Point,
 }
-
-// The following struct is for the activity.
-#[derive(Debug)]
-struct Matrix(f32, f32, f32, f32);
 
 fn main() {
-    // A tuple with a bunch of different types
-    let long_tuple = (1u8, 2u16, 3u32, 4u64, -1i8, -2i16, -3i32, -4i64, 0.1f32, 0.2f64, 'a', true);
+    // Instantiate a `Point`
+    let point: Point = Point { x: 0.3, y: 0.4 };
 
-    // Values can be extracted from the tuple using tuple indexing
-    println!("long tuple first value: {}", long_tuple.0);
-    println!("long tuple second value: {}", long_tuple.1);
+    // Access the fields of the point
+    println!("point coordinates: ({}, {})", point.x, point.y);
 
-    // Tuples can be tuple members
-    let tuple_of_tuples = ((1u8, 2u16, 2u32), (4u64, -1i8), -2i16);
+    // Destructure the point using a `let` binding
+    let Point { x: my_x, y: my_y } = point;
 
-    // Tuples are printable
-    println!("tuple of tuples: {:?}", tuple_of_tuples);
+    let _rectangle = Rectangle {
+        // struct instantiation is an expression too
+        p1: Point { x: my_y, y: my_x },
+        p2: point,
+    };
 
-    let pair = (1, true);
-    println!("pair is {:?}", pair);
+    // Instantiate a unit struct
+    let _nil = Nil;
 
-    println!("the reversed pair is {:?}", reverse(pair));
+    // Instantiate a tuple struct
+    let pair = Pair(1, 0.1);
 
-    // To create one element tuples, the comma is required to tell them apart
-    // from a literal surrounded by parentheses
-    println!("one element tuple: {:?}", (5u32,));
-    println!("just an integer: {:?}", (5u32));
+    // Access the fields of a tuple struct
+    println!("pair contains {:?} and {:?}", pair.0, pair.1);
 
-    //tuples can be destructured to create bindings
-    let tuple = (1, "hello", 4.5, true);
+    // Destructure a tuple struct
+    let Pair(integer, decimal) = pair;
 
-    let (a, b, c, d) = tuple;
-    println!("{:?}, {:?}, {:?}, {:?}", a, b, c, d);
-
-    let matrix = Matrix(1.1, 1.2, 2.1, 2.2);
-    println!("{}", matrix)
-
+    println!("pair contains {:?} and {:?}", integer, decimal);
 }

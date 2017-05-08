5368a437984d6ffb0faf311ff8e1e4361434c154
Markus Eriksson <markus.eriksson@nepa.com>
2017-05-08 18:15:20 +0200

    read file and print content

--- pokedex/src/main.rs
+++ pokedex/src/main.rs
@@ -1,3 +1,16 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world!");
+    let file = File::open("./src/data.txt")
+        .expect("Could not open file!");
+
+    let mut buff_reader = BufReader::new(file);
+
+    let mut content = String::new();
+    buff_reader.read_to_string(&mut content)
+        .expect("Could not read content to String");
+
+    println!("{}", content);
 }

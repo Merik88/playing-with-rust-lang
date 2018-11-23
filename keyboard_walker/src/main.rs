99cebd1333a44d689bb45fe237432b05ac60ecc1
Markus Eriksson <markus.eriksson@nepa.com>
2018-11-23 16:22:55 +0100

    append keyboard word to a word

--- keyboard_walker/src/main.rs
+++ keyboard_walker/src/main.rs
@@ -1,3 +1,23 @@
 fn main() {
-    println!("Hello, world!");
+    println!("{}", append_keyboard_word_to_word(String::from("Hello, "), String::from("World!")));
+}
+
+fn append_keyboard_word_to_word(word: String, keyboard_word: String) -> String {
+    let mut word_with_keyboard_word = String::from(word);
+    word_with_keyboard_word.push_str(keyboard_word.as_ref());
+    word_with_keyboard_word
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn should_append_keyboard_word_to_word() {
+        let word = String::from("Markus");
+        let keyboard_word = String::from("qwe");
+        
+        let expected_appended_word = String::from("Markusqwe");
+        assert_eq!(expected_appended_word, append_keyboard_word_to_word(word, keyboard_word));
+    }
 }

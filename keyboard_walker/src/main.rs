10ffa23e0255a152a8d3fc0607591d4892fc18b9
Markus Eriksson <markus.eriksson@nepa.com>
2018-12-03 14:38:59 +0100

    Read arguments passed in and make use of the new code

--- keyboard_walker/src/main.rs
+++ keyboard_walker/src/main.rs
@@ -1,14 +1,38 @@
-use std::env;
+extern crate strum;
+#[macro_use] extern crate strum_macros;
 
+use std::env;
+use std::str::FromStr;
+
+#[derive(EnumString)]
 enum Strategy {
     Horizontal,
     Vertical,
 }
 
 fn main() {
-    let args: Vec<String> = env::args().skip(1).collect();
-    let keyboard_word = vec![String::from(" World!")];
-    let new_words = append_keyboard_word_to_list_of_words(args, keyboard_word);
+    
+    if env::args().count() < 2 {
+        println!("1 arg: length of the generated word");
+        println!("2 arg: kayboard walk strategy (Horizontal or Vertical)");
+        println!("3 arg: words to append to");
+        return;
+    }
+    
+    let word_length = env::args().skip(1).take(1).collect::<String>().parse().unwrap();
+    let strategy = Strategy::from_str(&env::args().skip(2).take(1).collect::<String>()).unwrap();
+    let word_list = env::args().skip(3).collect();
+    let swedish_keyboard = vec![
+        "§1234567890+´".to_string(),
+        "qwertyuiopå¨".to_string(),
+        "asdfghjklöä'".to_string(),
+        "<zxcvbnm,.-".to_string(),
+    ];
+    
+    let new_keyboard_layout = create_keyboard_layout(swedish_keyboard, strategy);
+    let keyboard_word = generate_words_from_keyboard_layout(new_keyboard_layout, word_length);
+    let new_words = append_keyboard_word_to_list_of_words(word_list, keyboard_word);
+    
     for new_word in new_words.iter() {
         println!("{}", new_word);
     }
